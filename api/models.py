1c0005ec1970dc5424fa6d1007ad1f51df69429f
Tyler Hunt <tyler.s.hunt05@gmail.com>
2018-09-16 18:14:46 -0600

    playing around with some basics with models and the db

---
+++ api/models.py
@@ -0,0 +1,32 @@
+import datetime
+
+from django.db import models
+
+from django.core.validators import MaxValueValidator, MinValueValidator
+
+
+class Collection(models.Model):
+    '''
+    A collection of cards.
+    '''
+    name = models.CharField(max_length=250)
+
+
+class Card(models.Model):
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    team = models.CharField(max_length=100)
+    year = models.PositiveIntegerField(
+        validators=[
+            MinValueValidator(1887),  # first produced baseball card (source?)
+            MaxValueValidator(datetime.datetime.now().year),
+        ],
+        help_text='Use the following format: <YYYY>',
+    )
+    value = models.DecimalField(
+        decimal_places=2,
+        max_digits=50,
+        default=0,
+    )
+
+    collection = models.ManyToManyField(Collection)
