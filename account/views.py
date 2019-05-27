7647dd336e6c95378e518568b7de9d5bc8e4e54c
Tyler Hunt <tyler.s.hunt05@gmail.com>
2019-05-26 23:53:40 -0600

    added very basic signup form

--- account/views.py
+++ account/views.py
@@ -1,7 +1,11 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render
+
 from rest_framework import status
 from rest_framework.authtoken.models import Token
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .serializers import UserSerializer
 
@@ -22,3 +26,14 @@ class UserCreate(APIView):
                 return Response(ret_data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SignUp(APIView):
+    '''
+    User signup
+    '''
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'signup.html'
+
+    def get(self, request):
+        return render(request, 'signup.html', {'form': UserCreationForm()})
--- card_collector_db/urls.py
+++ card_collector_db/urls.py
@@ -4,7 +4,11 @@ cardcollectordb URL Configuration
 from django.contrib import admin
 from django.urls import include, path
 
+from account.views import SignUp
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls'), name='account'),
+    path('signup/', SignUp.as_view(), name='signup'),
 ]
